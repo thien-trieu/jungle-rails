813ce0f62cb4915dbb29b63e280ec7d81df1b59a
Thien Trieu <thien.trieu@telus.com>
2023-01-25 11:00:44 -0800

    Create dashboard with product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_UN'], password: ENV['HTTP_AUTH_PW']
+  
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
