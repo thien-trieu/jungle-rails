f4e22074ba3cff31374ea39ea9ff787226ad763a
Thien Trieu <thien.trieu@telus.com>
2023-01-30 20:16:53 -0800

    Create cypress test for Product Detail Navigation

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,20 @@
+describe('Product Details', () => {
+  beforeEach(() => {
+   
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Should navigate to Product Details", () => {
+    cy.get(':nth-child(1) > a > img')
+      .click()
+  });
+
+})
